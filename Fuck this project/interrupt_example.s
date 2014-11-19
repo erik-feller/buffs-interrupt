be2eb7b07f9f9c39c857694c14738d09cd6e6cd8
unknown <erfe5132@SRC-OPTI990-02.ad.colorado.edu>
2014-11-18 20:14:27 -0700

    This is what ive got so far adding initial commit

---
+++ Fuck this project/interrupt_example.s
@@ -0,0 +1,94 @@
+.include "key_codes.s"			/* defines values for KEY1, KEY2, KEY3 */
+/********************************************************************************
+ * This program demonstrates use of interrupts in the DE0 Basic Computer. It
+ * first starts the interval timer with 33 msec timeouts, and then enables 
+ * Nios II interrupts from the interval timer and pushbutton KEYs
+ *
+ * The interrupt service routine for the interval timer displays a pattern on 
+ * the HEX displays, and shifts this pattern either left or right. The shifting
+ * direction is set in the pushbutton interrupt service routine, as follows:
+ *		KEY[1]: shifts the displayed pattern to the right
+ *		KEY[2]: shifts the displayed pattern to the left
+********************************************************************************/
+	.text									/* executable code follows */
+	.global _start
+_start:
+	/* set up the stack */
+	movia 	sp, 0x007FFFFC			/* stack starts from largest memory address */
+
+	movia		r16, 0x10002000		/* internal timer base address */
+	movia		r9, 0x10000000
+	movia		r10, 0x0000001F
+	stwio 		r10, 0(r9)
+
+	/* set the interval timer period for scrolling the HEX displays */
+	movia		r12, 0x00F00000			/* 1/(50 MHz) x (0x190000) = 33 msec */
+	sthio		r12, 8(r16)				/* store the low half word of counter start value */ 
+	srli		r12, r12, 16
+	sthio		r12, 0xC(r16)			/* high half word of counter start value */ 
+
+	/* start interval timer, enable its interrupts */
+	movi		r15, 0b0111				/* START = 1, CONT = 1, ITO = 1 */
+	sthio		r15, 4(r16)
+
+	/* write to the pushbutton port interrupt mask register */
+	movia		r15, 0x10000050		/* pushbutton key base address */
+	movi		r7, 0b0110				/* set 2 interrupt mask bits (bit 0 is Nios II Reset) */
+	stwio		r7, 8(r15)				/* interrupt mask register is (base + 8) */
+
+	/* enable Nios II processor interrupts */
+	movi		r7, 0b011				/* set interrupt mask bits for levels 0 (interval */
+	wrctl		ienable, r7				/* timer) and level 1 (pushbuttons) */
+	movi		r7, 1
+	wrctl		status, r7				/* turn on Nios II interrupt processing */
+	movia		r19, HEX_bits
+	addi		r18, r19, 84
+
+IDLE:
+	br 		IDLE						/* main program simply idles */
+
+	.data
+/* The two global variables used by the interrupt service routines for the interval timer
+ * and the pushbutton keys are declared below */
+	.global	PATTERN
+PATTERN:
+	.word		0xFFFFFFFF				/* pattern to show on the HEX displays */
+	
+	.global	KEY_PRESSED
+	.global SPEED
+SPEED:
+	.word 0x00000003
+	
+	.global OFFSET
+OFFSET:
+	.word 0x00000000
+
+	.global HEX_bits
+HEX_bits:
+	.word 0x00063000
+	.word 0x00300600
+	.word 0x06000030
+	.word 0x30000006
+	.word 0x303d3f06
+	.word 0x00000000
+	.word 0x303d3f06
+	.word 0x00000000
+	.word 0x7c1c7171
+	.word 0x1c71716d
+	.word 0x71716d00
+	.word 0x716d0000
+	.word 0x6d000000
+	.word 0x00000000
+	.word 0x2908010d
+	.word 0x30090906
+	.word 0x1901080b
+	.word 0x2908010d
+	.word 0x30090906
+	.word 0x1901080b
+	.word 0x2908010d
+	.word 0x30090906
+	.word 0x1901080b
+KEY_PRESSED:
+	.word		KEY2						/* stores code representing pushbutton key pressed */
+
+	.end
